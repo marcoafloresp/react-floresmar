43afe1a0d9e0f37be2c215cdd65bb58b702aa11b
Marco Flores <floresmarcoantonio@gmail.com>
2023-02-16 22:42:56 -0400

    first commit

--- src/components/Navbar/cartwidget/CartWidget.js
+++ src/components/Navbar/cartwidget/CartWidget.js
@@ -4,6 +4,9 @@ const Cartwidget = () => {
             <div className="material-icons md-36 carrito-icon">
             shopping_cart
             </div>
+            <div className="material-symbols-outlined">
+counter_1
+</div>
         </div>
     )
 }
