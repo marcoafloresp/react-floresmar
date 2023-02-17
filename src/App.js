a720db5d37b9fa8f53264878c6e51146dd09cc2e
Marco Flores <floresmarcoantonio@gmail.com>
2023-02-16 22:04:36 -0400

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import NavBar from './components/Navbar/NavBar';
+import Contenedor from './components/contenedor/ItemListContainer';
+
+function App() {
+  return (
+    <div className="App">
+      <NavBar/>
+      <Contenedor/>
+    </div>
+  );
+}
+
+export default App;
